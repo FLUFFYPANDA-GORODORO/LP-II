8bbaf843f839d537b95e3e47d6a14a6dac63d9dc
Sumedh Sangle <sanglesumedh15@gmail.com>
2025-02-10 13:32:04 +0530

    Update chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot('Customer Bot')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+response = chatbot.get_response('What is your Number')
+print(response)|
+response = chatbot.get_response('Who are you?')
+print(response)
